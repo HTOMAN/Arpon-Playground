ac994b35c4e6551a95a7411c6e16c0b7c9e6ea8f
Jaime Costa Marques <jaime.mqu@gmail.com>
2021-01-06 09:15:27 -0300

    style: 💄 bg start

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
+import '../styles/tailwind.css'
 import '../styles/globals.css'
+import '../styles/utilities.css'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
