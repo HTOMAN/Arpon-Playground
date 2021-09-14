1c112d557483286dd264d5085480f9e1e5d8fce4
jaikme <jaime.mqu@gmail.com>
2021-09-14 10:16:16 -0300

    remove command and fix spelling

--- arpon/core/graphics/types/tests/CGPoint.test.ts
+++ arpon/core/graphics/types/tests/CGPoint.test.ts
@@ -31,14 +31,14 @@ describe('CGPoint', () => {
     expect(p.y).toBe(4);
   });
 
-  it('must conform to Equatable.equalTo to compare two Points nad return Truthy', () => {
+  it('must conform to Equatable.equalTo to compare two Points and return Truthy', () => {
     const pointA = new CGPoint(10, 20);
     const pointB = new CGPoint(10, 20);
     const result = pointA.equalTo(pointB);
     expect(result).toBeTruthy();
   });
 
-  it('must conform to Equatable.equalTo to compare two Points nad return Falsy', () => {
+  it('must conform to Equatable.equalTo to compare two Points and return Falsy', () => {
     const pointA = new CGPoint(10, 20);
     const pointB = new CGPoint(0, 0);
     const result = pointA.equalTo(pointB);
@@ -52,20 +52,3 @@ describe('CGPoint', () => {
     expect(coordinates).toContain(4455);
   });
 });
-
-/* const point1 = new CGPoint(1, 1);
-const point2 = new CGPoint(10, 10);
-const point3 = new CGPoint();
-const point4 = CGPoint.init({ x: 1, y: 1 });
-const point5 = CGPoint.zero;
-
-const point6 = new CGPoint(1, 1);
-const point7 = new CGPoint(10, 3);
-
-console.log(point1);
-console.log(point2);
-console.log(point3);
-console.log(point4);
-console.log(point5);
-console.log(point6.equalTo(point1));
-console.log(point7.equalTo(point2)); */
